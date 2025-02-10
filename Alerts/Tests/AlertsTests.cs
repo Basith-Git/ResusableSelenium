7dcda788a72ce09e879d07f82334e0cc4cf0d52a
Basith <Basith@DESKTOP-N5P0N3F>
2025-02-10 12:11:35 +0530

    Add project files.

---
+++ Alerts/Tests/AlertsTests.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResusableSelenium.Alerts.Tests
+{
+    internal class AlertsTests
+    {
+
+        [Test]
+        public void AlertsConfirmPopUp() {
+
+
+            var driver = new ChromeDriver();
+
+            driver.Manage().Window.Maximize();
+
+            driver.Url = "https://the-internet.herokuapp.com/javascript_alerts";
+
+            driver.FindElement(By.XPath("//button[.='Click for JS Alert']")).Click();
+
+            IAlert alert = driver.SwitchTo().Alert();
+
+            var alerttext = alert.Text;
+
+            alert.Accept();
+
+            Console.WriteLine(  alerttext);
+
+            driver.Close();
+        }
+    }
+}
